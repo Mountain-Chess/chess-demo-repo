fa32cef5ae3bb5e7555c8cfc07ec7835e956bb90
Stephen Hubbard <hubbard_stephen@yahoo.com>
2019-12-15 11:18:52 -0700

    random chess game in console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Chessboard from 'chessboardjsx';
+import Chess from 'chess.js';
 import Demo from "./Demo";
 
 
@@ -8,6 +9,15 @@ import Demo from "./Demo";
 export default class ChessExample extends Component {
   
   render() {
+    // random chess game
+    var chess = new Chess();
+
+      while (!chess.game_over()) {
+        var moves = chess.moves();
+        var move = moves[Math.floor(Math.random() * moves.length)];
+        chess.move(move);
+      }
+      console.log(chess.pgn());
     return (
       <div>
         <h1>test</h1>
