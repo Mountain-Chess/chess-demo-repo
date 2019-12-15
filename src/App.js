62ec9b796d59d9e023fef1d879477c91032b67e7
Stephen Hubbard <hubbard_stephen@yahoo.com>
2019-12-15 11:12:45 -0700

    repo displaying some 3rd party react libraries for chess boards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Chessboard from 'chessboardjsx';
+import Demo from "./Demo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class ChessExample extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>test</h1>
+        <Chessboard position="start"/>
+        <Demo />
+      </div>
+    )
+  }
 }
-
-export default App;
